7c1092099e18e7cc78c9bd1789c377e63ba761a0
Zeynep HAN <zeynephan9906@gmail.com>
2022-04-29 20:57:13 +0300

    29.04.2022

--- src/Lambda03.java
+++ src/Lambda03.java
@@ -12,6 +12,10 @@ public class Lambda03 {
         System.out.print("\n **** ");
         chrSayisiTersSirala(menu);
         System.out.print("\n **** ");
+        harfSayisi7denAzKontrol(menu);
+        System.out.print("\n **** ");
+        wIleBaslayanElKontrol(menu);
+        System.out.print("\n **** ");
 
     }
 
@@ -50,12 +54,42 @@ public class Lambda03 {
                 stream().
                 sorted(Comparator.comparing(t->t.toString().charAt(t.toString().length()-1)).reversed()).
                 forEach(t -> System.out.println(t + " "));
-        // Task : listin elemanlarin karakterlerinin cift sayili  karelerini hesaplayan,ve karelerini tekrarsiz buyukten kucuge sirali  print ediniz..
-        // Task : List elelmmalarinin karakter sayisini 7 ve 7 'den az olma durumunu kontrol ediniz.
-        // Task : List elelmanlarinin "W" ile baslamasını kontrol ediniz.
-        // Task : List elelmanlarinin "x" ile biten en az bir elemaı kontrol ediniz.
-        // Task : Karakter sayisi en buyuk elemani yazdiriniz.
-        // Task : list elemanlarini son harfine göre siralayıp ilk eleman hariç kalan elemanlari print ediniz.
+
+         }
+    // Task : listin elemanlarin karakterlerinin cift sayili  karelerini hesaplayan,ve karelerini tekrarsiz buyukten kucuge sirali  print ediniz..
+
+    public static void charKaresiCiftElSiralama(List<String> menu){
+        menu.
+                stream().
+                map(t->t.length()*t.length()).
+                filter(Lambda01::cftBul).
+                distinct().//benzersiz
+                sorted(Comparator.reverseOrder()).//ters buyukten kucuge
+                forEach(Lambda01::yazdir);
+    }
+    // Task : List elelmmalarinin karakter sayisini 7 ve 7 'den az olma durumunu kontrol ediniz.
+     public static void harfSayisi7denAzKontrol(List<String> menu){
+     boolean kontrol =  menu.
+                stream().allMatch(t->t.length()<=7);
+        if (kontrol){
+            System.out.println("List elemanlari 7 harften kucuk");
+        }else
+            System.out.println("List elemanlari 7 harften buyuk" );
+
+         System.out.println("kontrol = " + kontrol);
+         //anyMatch() --> enaz bir eleman sarti saglarsa true aksi durumda false return eder
+         //allMatch() --> tum  elemanlar sarti saglarsa true en az bir eleman sarti saglamazsa false return eder.
+         //noneMatch() --> hic bir sarti SAGLAMAZSA true en az bir eleman sarti SAGLARSA false return eder.
+     }
+
+    // Task : List elelmanlarinin "W" ile baslamasını kontrol ediniz.
+    public static void wIleBaslayanElKontrol(List<String> menu){
+        System.out.println(menu.stream().noneMatch(t->t.startsWith("w"))?" w ile baslayan yemegin menude var":"w ile baslayan yemegin menude yok");
+    //Yanlis sonuc
 
     }
+    // Task : List elelmanlarinin "x" ile biten en az bir elemaı kontrol ediniz.
+    // Task : Karakter sayisi en buyuk elemani yazdiriniz.
+    // Task : list elemanlarini son harfine göre siralayıp ilk eleman hariç kalan elemanlari print ediniz.
+
 }
